29e9b25608d1799a22bb7bb123d1605984dd97de
your-tears-amidst-the-desert <110824455+your-tears-amidst-the-desert@users.noreply.github.com>
2022-08-12 17:17:07 +0400

    Create Contest_2.java

---
+++ Contest_2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Contest_2 {
+
+    int Contest_2(int N) {
+        if(1 <= N & N <= 2 * Math.pow(10, 9)) {
+            if (N <= 1) {
+                return 0;
+            } else if (N % 2 == 0) {
+                return 1 + Contest_2(N / 2);
+            } else {
+                return 1 + Contest_2(N - 1);
+            }
+        }
+        else {
+            return 0;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int N = scan.nextInt();
+        Contest_2 ct2 = new Contest_2();
+        System.out.println(ct2.Contest_2(N));
+    }
+}
